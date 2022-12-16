d334684d5e8d66bf03a6dd9d767100673c72d632
Ken Zhu <k-zhu@onu.edu>
2022-12-16 04:48:12 -0500

    im gonna commit

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,25 +1,21 @@
 // After navigating away from home page to /chat page.
 // Make sure to redirect someone from /chat to the home page if they are not signed in.
 
-import React, { useEffect, useState } from 'react'
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import background from '../resources/chatpage.jpg';
 
 const ChatPage = () => {
-    const [chats, setChats] = useState([]);
-
-    const fetchChats = async () => {
-        const { data } = await axios.get('/api/chat');
-
-        setChats(data);
-    } ;
     
-    useEffect(() => {
-        fetchChats();
-    }, []);
-    
-  return <div>{chats.map(chat=> (
-  <div key = {chat._id}>{ chat.chatName }</div>
-  ))}</div>;
+    return <div style={{
+        width: "100%",
+        backgroundImage: `url(${background})`,
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+    }}>
+      
+  </div>;
 };
 
 export default ChatPage;
